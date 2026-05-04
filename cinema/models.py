f769f31eeb5b2b91a0ca92e67178bcbd1c6000a8
unknown <kgoncharenko83@gmail.com>
2026-05-04 19:58:56 +0200

    refactor: change if/else to raise exception

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.CharField(max_length=200)
     duration = models.IntegerField()
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from cinema.views import movie_list, movie_detail
 
-
 urlpatterns = [
-    path("", movie_list, name='movie_list'),
-    path("<int:pk>/", movie_detail, name='movie_detail'),
+    path("", movie_list, name="movie_list"),
+    path("<int:pk>/", movie_detail, name="movie_detail"),
 ]
 
 app_name = "cinema"
